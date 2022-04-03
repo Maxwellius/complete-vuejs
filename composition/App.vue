a42f2d822ff334101f79d1369a5d6e0c3f5794ed
William OLAX <olaxwilliam@gmail.com>
2022-04-03 20:07:55 +0200

    microblog start

---
+++ composition/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="a++"> {{ a }}</button>
+  <button @click="b++"> {{ b }}</button>
+  <p>{{ total }}</p>
+  <div 
+    v-for="trace in history"
+    :key="trace"
+  >
+    {{ trace }}
+  </div>
+</template>
+
+<script>
+import { ref, reactive, computed, watch, watchEffect } from 'vue'
+import { useNumbers } from './useNumbers.js'
+
+export default {
+  setup() {
+    return useNumbers();
+  }
+}
+</script>
+
+<style scoped>
+
+button {
+  height: 200px;
+  width: 200px;
+  font-size: 40px;
+}
+p {
+  font-size: 40px;
+}
+</style>
